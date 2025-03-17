67510e6e5af50b1de581639b8928ff3d099aec49
akayanuarcandra <hazumiya.yc@gmail.com>
2025-03-17 09:57:25 +0800

    add exception

--- src/Task2/Circle.java
+++ src/Task2/Circle.java
@@ -4,10 +4,18 @@ import java.util.Scanner;
 public class Circle {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("input radius: ");
-        double rad = scanner.nextDouble();
-        System.out.println("Circumstance of circle is: " + (Math.PI * rad * 2));
-        System.out.println("Area of circle is: " + (Math.PI * rad * rad));
+        while (true) {
+            try {
+                System.out.println("Input radius: ");
+                double rad = scanner.nextDouble();
+                System.out.println("Circumference of circle is: " + (Math.PI * rad * 2));
+                System.out.println("Area of circle is: " + (Math.PI * rad * rad));
+                break; // Exit the loop if input is valid
+            } catch (Exception e) {
+                System.out.println("Invalid input. Please enter a number.");
+                scanner.next(); // Clear the invalid input
+            }
+        }
         scanner.close();
     }
 }
